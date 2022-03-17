048b1b67058e1549b08b2e97ffce54661b98945e
CDwee <96454637+CDwee@users.noreply.github.com>
2022-03-16 17:18:49 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Started at 6:13 3-16-2022
+
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, shoer-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach')
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+// Stopped at 7:18 3-16-2022
